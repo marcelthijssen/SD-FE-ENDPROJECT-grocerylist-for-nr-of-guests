9227ba8a967ce938941dd04d09aa8e3debbc484e
marcelthijssen <info@studiothijssen.nl>
2022-01-27 16:12:15 +0100

    Single shoppinglist array added

--- src/components/recipeIngredientsList/RecipeIngredientsList.js
+++ src/components/recipeIngredientsList/RecipeIngredientsList.js
@@ -3,29 +3,28 @@ import displayAmount from "../../helpers/displayAmount";
 import styles from "../recipecard/RecipeCard.module.scss";
 
 function RecipeIngredientsList( { recipe, numberOfGuests } ) {
-  console.log(recipe);
+  console.log( recipe );
   return (
     <>
       { recipe && numberOfGuests &&
 
         <div>
           <h2>Ingredients:</h2>
-        <div className={styles["recipe-ingredients-info"]}>
+          <div className={ styles["recipe-ingredients-info"] }>
             <table>
               { recipe.extendedIngredients.map( ( ingredient ) =>
                 <tbody key={ ingredient.id }>
                 <tr>
-                  {/*Calculating amount adjusted from amount of guests input */}
+                  {/*Calculating amount adjusted from amount of guests input */ }
                   <td> { displayAmount( (ingredient.amount / recipe.servings) * numberOfGuests ) } </td>
                   <td> { ingredient.unit } </td>
                   <td> { ingredient.name }</td>
-                  push
                 </tr>
                 </tbody>
               ) }
             </table>
+          </div>
         </div>
-      </div>
 
       }
     </>
--- src/components/tableIngredients/TableIngredienst.js
+++ src/components/tableIngredients/TableIngredienst.js
@@ -7,52 +7,38 @@ import styles from "./TableIngredients.module.scss";
 function TableIngredients( { shoppingList } ) {
   console.log( shoppingList );
 
-    const shoppingArray = [];
+    const shoppingListAll = [];
     shoppingList.map( ( recipe ) => {
       recipe[0].extendedIngredients.map( ( ingredient ) => {
-        shoppingArray.push( {
+        shoppingListAll.push( {
           "id": ingredient.id,
           "name": ingredient.name,
           "unit": ingredient.unit,
           "amount": displayAmount( (ingredient.amount / recipe[0].servings) * recipe[1].numberofguests )
         } );
-        console.log( shoppingArray );
-
       } );
+      console.log( shoppingListAll );
     } );
 
     return (
       <>
-        { shoppingList &&
+        { shoppingListAll &&
           <div>
-            {
-              shoppingList.map( ( recipe ) =>
-                <div>
-                  <h3>{ recipe[0].title } for { recipe[1].numberofguests } guests</h3>
-
-                  <table key={ recipe.id } className={ styles["table-style"] }>
-
-                    <tr className={ styles["table-row1"] }>
-                      <th>amount</th>
-                      <th>unit</th>
-                      <th>Ingredient</th>
-                    </tr>
-
-                    { recipe[0].extendedIngredients.map( ( ingredient ) =>
-                      <tbody>
-                      <tr>
-                        <td
-                          className={ styles["table-row2"] }> { displayAmount( (ingredient.amount / recipe[0].servings) * recipe[1].numberofguests ) } </td>
-                        <td> { ingredient.unit }</td>
-                        <td>{ ingredient.name } </td>
-
-                      </tr>
-                      </tbody>
-                    ) }
-                  </table>
-                </div>
-              )
-            }
+            <h2>All ingredients for your shoppinglist:</h2>
+            <div className={ styles["recipe-ingredients-info"] }>
+              <table>
+                { shoppingListAll.map( ( ingredient ) =>
+                  <tbody key={ ingredient.id }>
+                  <tr>
+                    {/*Calculating amount adjusted from amount of guests input */ }
+                    <td> { ingredient.amount  } </td>
+                    <td> { ingredient.unit } </td>
+                    <td> { ingredient.name }</td>
+                  </tr>
+                  </tbody>
+                ) }
+              </table>
+            </div>
           </div>
         }
       </>
