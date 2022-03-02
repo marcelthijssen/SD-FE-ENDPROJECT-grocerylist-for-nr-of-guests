0a5806ba3b64dc1077751cd747eb8d4e78f2417d
marcelthijssen <info@studiothijssen.nl>
2022-03-02 23:05:30 +0100

    adding key

--- src/components/ShoppingListRecipes/ShoppingListRecipes.js
+++ src/components/ShoppingListRecipes/ShoppingListRecipes.js
@@ -1,11 +1,8 @@
 // TableIngredient is shown on /favorites
 
 import React from "react";
-// import displayAmount from "../../helpers/displayAmount";
 import styles from "./ShoppingListRecipes.module.scss";
 import { Link } from "react-router-dom";
-// import replaceUnitNames from "../../helpers/replaceUnitNames";
-// import toggleShoppingList from "../toggleShoppingList/ToggleShoppingList";
 
 function ShoppingListRecipes( { shoppingList } ) {
 
@@ -15,10 +12,8 @@ function ShoppingListRecipes( { shoppingList } ) {
         <div className={ styles["recipe-list"] }>
           <h4>Recipes included in your shoppinglist:</h4>
           { shoppingList.map( ( recipe ) =>
-
-            <p>
-              <Link  to={ `/recipe/${ recipe[0].id }` }>{ recipe[0].title }</Link>
-
+            <p >
+              <Link to={ `/recipe/${ recipe[0].id }` } key={recipe[0].title}>{ recipe[0].title }</Link>
           </p>
           ) }
         </div>
