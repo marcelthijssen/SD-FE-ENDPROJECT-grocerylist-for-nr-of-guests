df93ccff293580710b748351dce616af6adaa88f
marcelthijssen <info@studiothijssen.nl>
2022-05-31 14:17:23 +0200

    .

--- __test__/add.test.js
+++ __test__/add.test.js
@@ -1,11 +1,10 @@
 // test function to check if jest-test is working
 
-function sum(a, b) {
+function sum( a, b ) {
   return a + b;
 }
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-  console.log("Test is working");
-
-});
+test( "adds 1 + 2 to equal 3", () => {
+  expect( sum( 1, 2 ) ).toBe( 3 );
+  console.log( "Test is working" );
+} );
